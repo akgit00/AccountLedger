13bb160928185e88dc6e2c5dcb7fdd66f4fcb358
akgit00 <tkfsecond@gmail.com>
2025-10-13 18:59:04 -0400

    Reports continued...

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -18,6 +18,30 @@ public class Reports {
             System.out.print("Choose an option: ");
 
             String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("Month to Date report");
+                    break;
+                case "2":
+                    System.out.println("Previous Month report");
+                    break;
+                case "3":
+                    System.out.println("Year to Date report");
+                    break;
+                case "4":
+                    System.out.println("Previous Year report");
+                    break;
+                case "5":
+                    System.out.println("Search by Vendor");
+                    break;
+                case "0":
+                    viewingReports = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+
+            }
         }
     }
 }
