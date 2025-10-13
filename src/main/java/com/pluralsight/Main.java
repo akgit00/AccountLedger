d39a4c8010b33f4302d2d774a30a41c49110a8f4
akgit00 <tkfsecond@gmail.com>
2025-10-13 15:25:49 -0400

    First commit for main menu

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n==============================");
+            System.out.println("   ACCOUNTING LEDGER SYSTEM");
+            System.out.println("==============================");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Choose an option: ");
+
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+
+        }
+    }
+}
