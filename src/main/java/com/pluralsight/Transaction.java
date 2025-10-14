941860edb4b329361188eab076e4997ce09bb0a4
akgit00 <tkfsecond@gmail.com>
2025-10-14 09:02:37 -0400

    Getters for the Transaction class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -9,4 +9,26 @@ public class Transaction {
     private String description;
     private String vendor;
     private double amount;
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    // Getters
+    public LocalDate getDate() { return date; }
+    public LocalTime getTime() { return time; }
+    public String getDescription() { return description; }
+    public String getVendor() { return vendor; }
+    public double getAmount() { return amount; }
+
+
+    public String toString() {
+        return String.format("%s | %s | %-20s | %-15s | %10.2f",
+                date, time, description, vendor, amount);
+    }
 }
+
