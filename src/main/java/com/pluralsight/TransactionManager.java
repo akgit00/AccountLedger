9df1bdd0149c67c7af6d21710a7b044abc2f1e35
akgit00 <tkfsecond@gmail.com>
2025-10-14 11:01:19 -0400

    Reorganizing and removing placeholders

--- src/main/java/com/pluralsight/TransactionManager.java
+++ src/main/java/com/pluralsight/TransactionManager.java
@@ -9,12 +9,14 @@ import java.util.List;
 public class TransactionManager {
     private static final String FILE_PATH = "transactions.csv";
 
-    //This will be finished later to handle reading and writing to the CSV file
-    public static List<Transaction> loadTransactions(){
-        return new ArrayList<>(); //This will act as a placeholder for now while everything else gets set up
-    }
+    //This part will be able to save a transaction to the csv file
     public static void saveTransaction(Transaction transaction) {
         // Placeholder for saving to CSV
-    }
 
+        //This will be finished later to handle reading and writing to the CSV file
+        public static List<Transaction> loadTransactions () {
+            List<Transaction> transactions = new ArrayList<>();
+        }
+
+    }
 }
