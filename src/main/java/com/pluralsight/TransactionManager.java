54e8a5fc415b6b4dbaeedeb8af4720268a312317
akgit00 <tkfsecond@gmail.com>
2025-10-14 09:16:36 -0400

    Getting TransactionManager prepared for reading and writing to the CSV file

--- src/main/java/com/pluralsight/TransactionManager.java
+++ src/main/java/com/pluralsight/TransactionManager.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionManager {
+    private static final String FILE_PATH = "transactions.csv";
+
+    //This will be finished later to handle reading and writing to the CSV file
+    public static List<Transaction> loadTransactions(){
+        return new ArrayList<>(); //This will act as a placeholder for now while everything else gets set up
+    }
 
 
 }
