bb0f70be1faeb0413b41427dfd7a8e6558dafd70
akgit00 <tkfsecond@gmail.com>
2025-10-16 10:35:03 -0400

    Finding the color codes and set them as final strings

--- src/main/java/com/pluralsight/Utils.java
+++ src/main/java/com/pluralsight/Utils.java
@@ -4,8 +4,14 @@ public class Utils {
 
     //Color formatting will go here
 
+    //1. find the color codes and set them as final strings
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String YELLOW = "\u001B[33m";
+
     /*
-    1. find the color codes and set them as final strings
     2. create a method for the color amount and use an if else statement to set the rule
     3. print results
      */
