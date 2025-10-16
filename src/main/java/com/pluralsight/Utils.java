d95061a28867eead9629d930eef8abb7e59ef432
akgit00 <tkfsecond@gmail.com>
2025-10-16 10:04:05 -0400

    Pseudocode for the Utils class

--- src/main/java/com/pluralsight/Utils.java
+++ src/main/java/com/pluralsight/Utils.java
@@ -2,5 +2,11 @@ package com.pluralsight;
 
 public class Utils {
 
-    // Future date/time formatting and CSV helper methods go here
+    //Color formatting will go here
+
+    /*
+    1. find the color codes and set them as final strings
+    2. create a method for the color amount and use an if else statement to set the rule
+    3. print results
+     */
 }
